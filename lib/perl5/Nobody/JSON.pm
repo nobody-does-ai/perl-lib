6dac6876dc1adff1f7940d41701decaf66f4acc7
nobody <labhub@turing-trust.com>
2026-02-17 10:42:58 -0500

    expose the coder objects

--- lib/perl5/Nobody/JSON.pm
+++ lib/perl5/Nobody/JSON.pm
@@ -1,18 +1,38 @@
 package Nobody::JSON;
 use common::sense;
 require Exporter;
+BEGIN {
+  require JSON::PP;
+};
+BEGIN {
+  our($coder);
+  sub coder {
+    $coder //= JSON::PP->new->ascii->pretty->allow_nonref->convert_blessed;
+  };
+  sub encode_json($) {
+    coder()->encode(shift);
+  };
+  sub decode_json($) {
+    coder()->decode(shift);
+  };
+};
+BEGIN {
+  require JSON::XS;
+  require Nobody::PP;
+}
 our(@ISA) = qw(Exporter);
 our(@EXPORT) = ( qw( decode_json encode_json ));
 
-use JSON::XS qw(decode_json);
 our($coder);
-sub json_decode;
-sub json_encode;
+sub json_decode($);
 *json_decode=*decode_json;
 *json_encode=*encode_json;
-
-sub encode_json {
- $coder //= JSON::XS->new->ascii->pretty->allow_nonref;
+sub decode_json($) {
+ my ($coder)=coder;
+ return $coder->encode (shift);
+};
+sub encode_json($) {
+ my ($coder)=coder;
  return $coder->encode (shift);
 };
 1;

6bcf89eac7bbdf3d130979f77640f4ab9e671510
nobody <labhub@turing-trust.com>
2026-03-28 03:21:22 -0400

    x

--- lib/perl5/AnyEvent/ReadLine/Diverter.pm
+++ lib/perl5/AnyEvent/ReadLine/Diverter.pm
@@ -1,92 +1,28 @@
+package AnyEvent::ReadLine::Diverter;
+use Carp qw( croak );
 use AnyEvent::ReadLine::Gnu;
 use Tie::Handle;
-
-package Handle::Redir;
-require Tie::Handle;
+use Nobody::Util;
 
 our(@ISA) = qw(Tie::StdHandle);
 sub TIEHANDLE {
   my $pkg = shift;
-  if (defined &{"{$pkg}::new"}) {
-    warnings::warnif("WARNING: calling ${pkg}->new since ${pkg}->TIEHANDLE is missing");
-    $pkg->new(@_);
-  }
-  else {
-    croak "$pkg doesn't define a TIEHANDLE method";
-  }
+  my $self={};
+  open($self->{sink},">&".fileno(@_?shift:*STDOUT));
+  bless($self,$pkg);
 }
-
-sub PRINT {
-  my $self = shift;
-  if($self->can('WRITE') != \&WRITE) {
-    my $buf = join(defined $, ? $, : "",@_);
-    $buf .= $\ if defined $\;
-    $self->WRITE($buf,length($buf),0);
-  }
-  else {
-    croak ref($self)," doesn't define a PRINT method";
-  }
-}
-
-sub PRINTF {
-  my $self = shift;
-
-  if($self->can('WRITE') != \&WRITE) {
-    my $buf = sprintf(shift,@_);
-    $self->WRITE($buf,length($buf),0);
-  }
-  else {
-    croak ref($self)," doesn't define a PRINTF method";
-  }
-}
-
-sub READLINE {
-  my $pkg = ref $_[0];
-  croak "$pkg doesn't define a READLINE method";
-}
-
-sub GETC {
-  my $self = shift;
-
-  if($self->can('READ') != \&READ) {
-    my $buf;
-    $self->READ($buf,1);
-    return $buf;
-  }
-  else {
-    croak ref($self)," doesn't define a GETC method";
-  }
-}
-
-sub READ {
-  my $pkg = ref $_[0];
-  croak "$pkg doesn't define a READ method";
-}
-
+our($done);
 sub WRITE {
-  my $pkg = ref $_[0];
-  croak "$pkg doesn't define a WRITE method";
+  my($self)=shift;
+  if($done) {
+    $self->{sink}->print(substr($_[0],$_[2],$_[1]));
+  } else {
+    local($done)=1;
+    AnyEvent::ReadLine::Gnu->print(pp(\@_));
+  };
 }
 
-sub CLOSE {
-  my $pkg = ref $_[0];
-  croak "$pkg doesn't define a CLOSE method";
-}
-
+tie *STDOUT, 'AnyEvent::ReadLine::Diverter', *STDOUT;
+tie *STDERR, 'AnyEvent::ReadLine::Diverter', *STDERR;
 1;
 
-sub READ { ... }		# Provide a needed method
-sub TIEHANDLE { ... }	# Overrides inherited method
-
-# asynchronously print something
-my $t = AE::timer 1, 1, sub {
-  $rl->hide;
-  print "async message 1\n"; # mind the \n
-  $rl->show;
-
-  # the same, but shorter:
-  $rl->print ("async message 2\n");
-};
-
-# do other eventy stuff...
-AE::cv->recv;
